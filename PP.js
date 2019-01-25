ae24e4a85b78e8b9302e8ff07875d4be600a5fa0
Nirvamada <47027590+Nirvamada@users.noreply.github.com>
2019-01-25 11:42:36 +0100

    Add files via upload

---
+++ PP.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#SB").hover(function(){
+        $("#p-SB").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#BMY").hover(function(){
+        $("#p-BMY").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#BSM").hover(function(){
+        $("#p-BSM").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#design").hover(function(){
+        $("#design").css("transform","translate(200px,0)");
+        $("#design").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $("#development").hover(function(){
+        $("#development").css("transform","translate(200px,0)");
+        $("#development").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $("#marketing").hover(function(){
+        $("#marketing").css("transform","translate(200px,0)");
+        $("#marketing").css("opacity", 1);
+
+    });
+});
